e732a6a2291efaabe102a216b0a27e8632a3c1d2
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-03 11:30:52 +0545

    Authprovider setup done to protect routes

---
+++ src/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log("here",isLoggedIn)
+
+  const logIn = () => {
+    console.log('logIn function called');
+    setIsLoggedIn(true);
+  };
+
+  const logOut = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
